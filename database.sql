96735fd1f998de1fe701ed5db67e3496182ba7fd
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2023-08-29 10:50:54 +0530

    db

--- database.sql
+++ database.sql
@@ -1,18 +1,18 @@
 
 CREATE TABLE `Customers` (
   `CustID` INT PRIMARY KEY AUTO_INCREMENT,
-  `FullName` varchar(10) NOT NULL,
-  `MobileNo` varchar(10),
+  `FullName` varchar(30) NOT NULL,
+  `MobileNo` varchar(30),
   `Address` text,
-  `City` varchar(10),
-  `Password` varchar(8) 
+  `City` varchar(30),
+  `Password` varchar(10) 
 );
 
 
 CREATE TABLE `Dishes` (
   `DishID` INT PRIMARY KEY AUTO_INCREMENT,
-  `DishName` varchar(10) NOT NULL,
-  `DishType` varchar(10),
+  `DishName` varchar(30) NOT NULL,
+  `DishType` varchar(30),
   `DishCategory` text,
   `DishPrice` FLOAT,
   `DishImage` text 
@@ -21,8 +21,8 @@ CREATE TABLE `Dishes` (
 CREATE TABLE `Orders` (
   `OrderID` INT PRIMARY KEY AUTO_INCREMENT,
   `OrderDate` date DEFAULT CURRENT_DATE(),
-  `DishName` varchar(20),
-  `DishType` varchar(10),
+  `DishName` varchar(30),
+  `DishType` varchar(30),
   `DishCategory` text,
   `DishPrice` FLOAT,
   `DishQty` INT 
